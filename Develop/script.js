0602b880ba282eb9e186940b9ca674b87394d44b
Nick <145059598+Dev-Attaway@users.noreply.github.com>
2023-11-06 15:40:59 -0800

    Update script.js
    Fixed grammar mistakes

--- Develop/script.js
+++ Develop/script.js
@@ -45,17 +45,17 @@ function readEventTextFromStorage() {
   // if somebtnNumber was succefully loaded in description then 
   // JSON.parse(discription) transfroms the strings loaded from
   //  local storage into objects
-  if (discription) {
+  if (description) {
     discription = JSON.parse(discription);
   }
 
-  // returns an empty array if discription = falsey
+  // returns an empty array if description = falsey
   // meaning that there was nobtnNumber in local storage to load 
   else {
-    discription = [null, null, null, null, null, null, null, null, null];
+    description = [null, null, null, null, null, null, null, null, null];
   }
 
-  return discription;
+  return description;
 }
 
 function handleDeleteProject() {
@@ -149,7 +149,7 @@ function setStyle() {
       // converts to a number
       time *= 1;
 
-      // converts the number into a base 23 time system, think military time 
+      // converts the number into a base 23-time system, think military time 
       time += 12;
     }
     else {
@@ -171,29 +171,11 @@ function setStyle() {
   }
 }
 
-// timeSlot.children('div').eq(i).text()
-// We are targeting the time-block class, within this class there are 3
-// children, we only want the first one, and we only want the text value of that 
-// particular child 
-
 $(function () {
 
   init();
   setStyle();
 });
-
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-
-
-// TODO: Add code to get any user input that was saved in localStorage and set
-// the values of the corresponding textarea elements. HINT: How can the id
-// attribute of each time-block be used to do this?
-//
 
 
 
